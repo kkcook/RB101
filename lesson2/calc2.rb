69a7476c5e4b8dcec2b9b8ef461e6c353c190e43
Karissa Cook <karissa.cook@me.com>
2020-09-09 11:18:00 -0700

    added calculator exercise

---
+++ lesson2/calc2.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# calc.rb
+
+def prompt(message)
+  puts "==> #{message}"
+end
+
+def valid_number?(num)
+  num == num.to_i.to_s
+end
+
+loop do
+  num1 = ''
+  loop do
+    prompt('Enter first num:')
+    num1 = gets.chomp
+    if valid_number?(num1)
+      break
+    else
+      prompt('Please enter a valid number.')
+    end
+  end
+
+  prompt('Enter operator:')
+  operator = gets.chomp
+
+  num2 = ''
+  loop do
+    prompt('Enter second num:')
+    num2 = gets.chomp
+    if valid_number?(num2)
+      break
+    else
+      prompt('Please enter a valid number.')
+    end
+  end
+
+  output = case operator
+           when '/'
+             num1.to_f / num2
+           when '*'
+             num1.to_i * num2.to_i
+           when '+'
+             num1.to_i + num2.to_i
+           when '-'
+             num1.to_i - num2.to_i
+           else
+             'Invalid operator, try again.'
+  end
+
+  prompt(output)
+
+  prompt("Would you like to keep calculating? Type 'y' if so.")
+  continue = gets.chomp.downcase
+  break if continue[0] != 'y'
+end
+
+# test
