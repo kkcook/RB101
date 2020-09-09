e2f28a1d9aa428400c4fcebba288e88c69ffe3fe
Karissa Cook <karissa.cook@me.com>
2020-09-09 13:42:37 -0700

    file creation, outline of pedac problem

---
+++ lesson2/mortgage_calc.rb
@@ -0,0 +1,13 @@
+=begin 
+P: Create a mortgage/car loan repayment calculator
+- Inputs: loan amount (int/float), interest rate (float), loan duration (int)
+- Outputs: monthly payment (float)
+
+Formula to use: m = p * (j / (1 - (1 + j)**(-n))) 
+    m = monthly payment
+    p = loan amount
+    j = monthly interest rate
+    n = loan duration in months
+=end
+
+
