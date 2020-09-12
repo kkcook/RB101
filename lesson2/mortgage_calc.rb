6f03afc3e63a304002e3960d4d4929e0cb027f8d
Karissa Cook <karissa.cook@me.com>
2020-09-11 20:43:52 -0700

    updating files

--- lesson2/mortgage_calc.rb
+++ lesson2/mortgage_calc.rb
@@ -10,4 +10,24 @@ Formula to use: m = p * (j / (1 - (1 + j)**(-n)))
     n = loan duration in months
 =end
 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_messages.yml')
+
+def prompt(message)
+  puts "--> #{message}"
+end
+
+def valid_number?(num)
+  num == num.to_i.to_S
+end
+
+prompt(MESSAGES['welcome'])
+prompt(MESSAGES['ask_name'])
+name = gets.chomp
+prompt("Hi, #{name}!")
+
+loop do 
+prompt(MESSAGES['loan'])
+loan_ammount = gets.chomp #make sure it's a valid number
+end
 
